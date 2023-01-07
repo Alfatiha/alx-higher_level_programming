eb397579cfaed4c7ccf5e9aab6dcc4907b11de7c
Alfatiha <alfatihaahmed@gmail.com>
2023-01-07 11:30:37 +0000

    3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for ch in range(97, 123):
-    if ch == 101 and 113:
+    if ch == 101:
+        continue
+    elif ch == 113:
         continue
     print("{0:s}".format(chr(ch)), end='')
